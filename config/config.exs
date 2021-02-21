e6d194f7842502d13650451700dcb7dc5a4a9f1b
Thomas Brus <thomas.brus@me.com>
2021-02-21 01:15:13 +0100

    Cleaned up configuration files

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :webhook_bot, WebhookBot.Scheduler,
-  jobs: [
-    {"*/5 * * * *", {WebhookBot, :call_endpoint, []}}
-  ]
+  jobs: [{"*/5 * * * *", {WebhookBot, :call_endpoint, []}}]
+
+config :webhook_bot, :endpoint_url, "http://example.com/"
